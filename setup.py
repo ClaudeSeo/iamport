4daa08d27d093bedf819ebca0ba455d6060443a6
SeoDongMyeong <ehdaudtj@gmail.com>
2016-08-19 14:30:06 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='iamport',
+    version='0.0.1',
+    py_modules=['iamport'],
+    author='ClaudeSeo',
+    author_email='ehdaudtj@gmail.com',
+    url='https://github.com/SeoDongMyeong/iamport',
+    description='Iamport Library'
+)
